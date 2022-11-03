0f70644cc88e9375fa4e52a5bfd47e43136b1695
Tom Hazledine <tom@tomhazledine.com>
2022-11-03 12:41:10 +0000

    amending file path for build directory

--- src/poc.js
+++ src/poc.js
@@ -6,7 +6,7 @@ import { copyFile, readFile, readFolder, saveFile } from "./io.js";
 import { markdown } from "./markdown.js";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
-const IN_DIRECTORY = path.resolve(__dirname, "../content/drafts");
+const IN_DIRECTORY = path.resolve(__dirname, "../content/");
 const OUT_DIRECTORY = path.resolve(__dirname, "../build/");
 
 const processFile = async filePath => {
@@ -15,14 +15,19 @@ const processFile = async filePath => {
     // Ignore weird files (i.e. `.DS_Store` etc.)
     if (extension === "") return;
 
-    const outPath = filePath.replace(IN_DIRECTORY, OUT_DIRECTORY);
+    const copyPath = filePath.replace(IN_DIRECTORY, OUT_DIRECTORY);
 
     if (extension === ".md") {
+        const updatePath = path.join(
+            path.dirname(copyPath),
+            path.basename(copyPath, path.extname(copyPath)) + ".html"
+        );
+
         const fileContents = await readFile(filePath);
         const markdownContents = markdown.render(fileContents);
-        saveFile(outPath, markdownContents);
+        saveFile(updatePath, markdownContents);
     } else {
-        copyFile(filePath, outPath);
+        copyFile(filePath, copyPath);
     }
 
     // // If the file isn't an image format that we want
