11b018d3417cb19aec6e97e8e277b26af61b52b3
Tom Hazledine <tom@tomhazledine.com>
2022-12-05 12:57:19 +0000

    docs: first pass at Getting Started content

---
+++ docs/templates/components/Callout.jsx
@@ -0,0 +1,18 @@
+import { markdown } from "jsssg";
+
+const Callout = ({ title, children }) => {
+    return (
+        <div className="callout">
+            <span className="callout__eyebrow">Author's note</span>
+            {title && (
+                <h3 dangerouslySetInnerHTML={{ __html: markdown(title) }} />
+            )}
+            <div
+                className="markdown-stuff"
+                dangerouslySetInnerHTML={{ __html: markdown(children) }}
+            />
+        </div>
+    );
+};
+
+export default Callout;
