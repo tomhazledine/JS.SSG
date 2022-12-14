472b9b47d6b02063a56e6b363ef30f23e201eec2
Tom Hazledine <tom@tomhazledine.com>
2022-12-14 10:55:09 +0000

    adding postcss (autoprefixer and cssnano) to style compilation

--- jsssg/src/sass.js
+++ jsssg/src/sass.js
@@ -1,16 +1,26 @@
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
+import postcss from "postcss";
 import sass from "sass";
 
 import { log } from "./console.js";
 import { args } from "./index.js";
 import { saveFile } from "./io.js";
 
-export const compileSass = (stylePath, PATHS) => {
-    const result = sass.compile(stylePath);
+export const compileSass = async (stylePath, PATHS) => {
+    const rawCSS = sass.compile(stylePath);
+
+    const compiledCSS = await postcss([autoprefixer, cssnano]).process(
+        rawCSS.css,
+        {
+            from: stylePath
+        }
+    );
 
     const outPath = stylePath
         .replace(PATHS.STYLES, PATHS.OUT)
         .replace(".scss", ".css");
 
     if (args.verbose) log(`Writing css to ${outPath}`, "green");
-    saveFile(outPath, result.css);
+    saveFile(outPath, compiledCSS.css);
 };
