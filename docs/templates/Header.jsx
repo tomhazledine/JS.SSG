5c6bf1bc3c3dd74ba275f857b58429353b5a2231
Tom Hazledine <tom@tomhazledine.com>
2022-12-14 10:55:36 +0000

    fixing text gradients for Safari

--- docs/templates/Header.jsx
+++ docs/templates/Header.jsx
@@ -2,7 +2,8 @@ const Header = ({ title }) => (
     <header>
         <h2 className="header__title">
             <a href="/">
-                {title} <span className="header__title__suffix">Docs</span>
+                <span className="header__title--gradient">{title}</span>{" "}
+                <span className="header__title__suffix">Docs</span>
             </a>
         </h2>
     </header>
