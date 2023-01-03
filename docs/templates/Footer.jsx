b8c4bc60d8e9b6226dd5800dd489c5725fe25baf
Tom Hazledine <tom@tomhazledine.com>
2023-01-03 14:17:39 +0000

    docs: analytics

---
+++ docs/templates/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = ({}) => (
+    <>
+        <script
+            defer
+            data-domain="jsssg.org"
+            src="https://plausible.io/js/script.js"
+        ></script>
+    </>
+);
+
+export default Footer;
--- docs/templates/Wrapper.jsx
+++ docs/templates/Wrapper.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header.js";
+import Footer from "./Footer.js";
 import Menu from "./Menu.js";
 
 const Wrapper = ({ page = {}, site = {}, children }) => (
@@ -54,6 +55,7 @@ const Wrapper = ({ page = {}, site = {}, children }) => (
             <main className="main">
                 <div className="container stack">{children}</div>
             </main>
+            <Footer />
         </body>
     </html>
 );
