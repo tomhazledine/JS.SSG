8d40eb9e6fc669f978d00c4ad22abaf51a1bfb12
Tom Hazledine <tom@tomhazledine.com>
2023-06-09 13:59:15 +0100

    feat: app appName to style paths

--- jsssg/src/sass.js
+++ jsssg/src/sass.js
@@ -1,5 +1,6 @@
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
+import path from "path";
 import postcss from "postcss";
 import sass from "sass";
 
@@ -7,7 +8,7 @@ import { log } from "./console.js";
 import { args } from "./index.js";
 import { saveFile } from "./io.js";
 
-export const compileSass = async (stylePath, PATHS) => {
+export const compileSass = async (stylePath, PATHS, appPrefix = false) => {
     let rawCSS;
     try {
         rawCSS = sass.compile(stylePath);
@@ -24,9 +25,13 @@ export const compileSass = async (stylePath, PATHS) => {
         }
     );
 
+    const pathMeta = path.parse(stylePath);
+
+    const prefix = appPrefix ? `${appPrefix}.` : "";
+
     const outPath = stylePath
         .replace(PATHS.STYLES, PATHS.OUT)
-        .replace(".scss", ".css");
+        .replace(pathMeta.base, `${prefix}${pathMeta.name}.css`);
 
     if (args.verbose) log(`Writing css to ${outPath}`, "green");
     saveFile(outPath, compiledCSS.css);
